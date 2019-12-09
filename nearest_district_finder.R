60aacae84bda9378499acd84235814d7c7f91dfd
Jonghyun Lee <green198800@yonsei.ac.kr>
2019-12-09 21:20:27 +0900

    ggplot으로 백화점 위치 서울 지도에 플랏하기

--- nearest_district_finder.R
+++ nearest_district_finder.R
@@ -1,14 +1,17 @@
 rm(list = ls())
 
+setwd("~/GitRepo/Multicampus_semi/address")
+
 data = read.csv("distance_matrix.csv")
 data = data[-1]
 
 header_data = read.csv("집계코드_위도_경도_주소.csv", fileEncoding = "CP949", encoding = "euc-kr")
 options("scipen" = 100)
 header = header_data[,2]
+head(header_data)
 
 department = read.csv("점포이름_위도_경도_주소.csv", fileEncoding = "CP949", encoding = "euc-kr")
-
+head(department)
 
 # sum(data == 0)
 # my_mat = matrix(rep(0,length(data)*5), nrow=5)
@@ -21,42 +24,36 @@ for (dept in 1:length(data)){
   idx = order(dat)
   
   my_mat = cbind(my_mat, head(header[idx]), head(sort(dat)))
-  print(my_mat[,dept])
+  # print(my_mat[,dept])
 }
 
-x = department$lon[1]
-y = department$lat[1]
-# 
-# 
-# 
 # x = department[header_data[,2] == my_mat[1,1], 3]
 # y = header_data[header_data[,2] == my_mat[1,1],4]
-
-
 
 library(rgdal)
 library(ggplot2)
 
 my_spdf <- readOGR( 
   dsn= paste0("../map/") , 
-  layer="seoul",
-  verbose=FALSE
+  layer="seoul"
 )
 
-target = header_data[header_data[,2] == my_mat[1,1], 2]
-my_coord = my_spdf[,as.integer(my_spdf@data[, "TOT_REG_CD"]) == my_mat[1,1]]
-plot(my_coord, col="#f2f2f2", bg="skyblue", border=0 )
+# 한국 중부 좌표계
+from_crs = CRS("+proj=tmerc +lat_0=38 +lon_0=127.5 +k=0.996 +x_0=1000000 +y_0=2000000 +ellps=GRS80 +units=m")
+# 세계 표준 좌표계
+to_crs = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
 
-# 'fortify' the data to get a dataframe format required by ggplot2
-# library(broom)
-# spdf_fortified <- tidy(my_spdf, region = "NAME")
+proj4string(my_spdf) = from_crs
 
+converted = spTransform(my_spdf, to_crs)
+# converted
 
-plot(my_spdf, col="#f2f2f2", bg="skyblue", border=0 )
+plot(converted, axes = T)
+
+# target = header_data[header_data[,2] == my_mat[1,1], 2]
+# my_coord = my_spdf[,as.integer(my_spdf@data[, "TOT_REG_CD"]) == my_mat[1,1]]
 
 library(rgdal)
-proj4string(my_spdf) <- CRS("+proj=longlat +ellps=WGS84")
-shapefile <- spTransform(my_spdf, CRS("+proj=longlat +datum=WGS84")) #change CRS
 
 # Plot it
 fig = ggplot() +
@@ -66,16 +63,16 @@ fig = ggplot() +
 fig 
 
 ggplot() +
-  geom_polygon(data = my_spdf, aes( x = long, y = lat, group = group), fill="#69b3a2", color="white") +
-  theme_void()
+  geom_polygon(data = converted, aes( x = long, y = lat, group = group), fill="#69b3a2", color="white") +
+  theme_void() + geom_point(data = department, aes(lon, lat))
 
-depart = department[1,]
-
-ggplot() +
-  geom_polygon(data = temp, aes( x = long, y = lat, group = group), fill="#69b3a2", color="white") +
-  geom_point(data = depart, aes(x = lon, y = lat))
-
-
-ggplot()+
-  geom_point(data = department, aes(lon, lat))
+# depart = department[1,]
+# 
+# ggplot() +
+#   geom_polygon(data = temp, aes( x = long, y = lat, group = group), fill="#69b3a2", color="white") +
+#   geom_point(data = depart, aes(x = lon, y = lat))
+# 
+# 
+# ggplot()+
+#   geom_point(data = department, aes(lon, lat))
 
